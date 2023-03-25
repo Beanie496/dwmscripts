acd6e5b118494bed362fc344e47e97967f345ce9
Jasper Shovelton <jrshovelton@gmail.com>
2023-03-25 14:45:55 +0000

    add comments and account for line wrapping

--- dwmbl_cpubar.c
+++ dwmbl_cpubar.c
@@ -19,6 +19,8 @@
 #define LENGTH(arr)  (sizeof(arr) / sizeof(arr[0]))
 
 static char line[MAXLINE];
+// this stores 9 characters: a space,
+// then the 8 block elements that go from lower eighth to full block
 static const wchar_t bars[] = { 0x0020, 0x2581, 0x2582, 0x2583, 0x2584, 0x2585, 0x2586, 0x2587, 0x2588 };
 
 struct {
@@ -37,7 +39,9 @@ char *getLine(FILE *fp);
 
 int main(int argc, char *argv[])
 {
+	// this allows the wide chars to be used
 	setlocale(LC_ALL, "");
+
 	if (getCoreInfo())
 		return 0;
 	showTotalCPUTime();
@@ -49,8 +53,10 @@ int main(int argc, char *argv[])
 
 void showTotalCPUTime()
 {
-	double fraction = (double)(100 * coreStats.idleTime) / (double)(coreStats.elapsedTime * coreStats.totalCores);
-	// I don't know why, but somwtimes the idle time diff is higher than the elapsed time diff
+	double fraction = (double)(100 * coreStats.idleTime) /
+		(double)(coreStats.elapsedTime * coreStats.totalCores);
+	// I don't know why, but somwtimes the idle time diff is higher than
+	// the elapsed time diff
 	if (fraction > 100.0)
 		fraction = 100.0;
 	printf("CPU: %.2f%%", 100.0 - fraction);
@@ -60,11 +66,12 @@ void showTotalCPUTime()
 void showVisualCores()
 {
 	int fraction;
-
 	printf(" ");
 	for (int i = 0; i < coreStats.totalCores; i++) {
-		fraction = LENGTH(bars) - 1 - (int)(8 * coreStats.idleTimes[i] / coreStats.elapsedTime);
-		// same as before. The idle time diff is sometimes higher than the total elapsed time diff.
+		fraction = LENGTH(bars) - 1 -
+			(int)(8 * coreStats.idleTimes[i] / coreStats.elapsedTime);
+		// same as before. The idle time diff is sometimes higher than
+		// the total elapsed time diff.
 		if (fraction < 0)
 			fraction = 0;
 		printf("%lc", bars[fraction]);
@@ -82,25 +89,29 @@ int getCoreInfo()
 	int cacheTime, cacheIdle;
 	int ticksPerSec = sysconf(_SC_CLK_TCK);
 	int cores = sysconf(_SC_NPROCESSORS_ONLN);
-	int tmp[cores];
+	int cachedCoreIdleTimes[cores];
 
 	coreStats.totalCores = cores;
 	coreStats.idleTimes = malloc(sizeof(int *) * cores);
-	// I get the total uptime, the total idle time and the idle time
-	// of each core as close together as possible because the idle time
-	// of some of the cores might increase between getting the uptime
-	// and getting the idle time of that core
+	// the total uptime, the total idle time and the idle time
+	// of each core are read in as close together as possible because
+	// the idle time of some of the cores might increase between getting
+	// the total uptime and getting the idle time of it
 	fscanf(uptimeFile, "%lf %lf", &uptime, &idle);
 	// ignore the first line, as that stores the collective core info
-	fscanf(stats, "%*s %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d");
+	getLine(stats);
+	// get the idle time of each core (the 5th column)
 	for (int i = 0; i < cores; i++)
 		fscanf(stats, "%*s %*d %*d %*d %d %*d %*d %*d %*d %*d %*d", &coreStats.idleTimes[i]);
 	fclose(uptimeFile);
 	fclose(stats);
 
+	// this doesn't truncate the decimal by the way
 	coreStats.elapsedTime = uptime * ticksPerSec;
 	coreStats.idleTime = idle * ticksPerSec;
 
+	// if the cache doesn't exist, store the elapsed time, idle time,
+	// and idle time of each core (all in clock ticks)
 	if (cache == (FILE *)NULL) {
 		cache = fopen("/tmp/cpubarcache", "w");
 		fprintf(cache, "%d %d", coreStats.elapsedTime, coreStats.idleTime);
@@ -110,26 +121,31 @@ int getCoreInfo()
 		return 1;
 	}
 
+	// get all the info from the cache before writing in the current info
 	fscanf(cache, "%d %d", &cacheTime, &cacheIdle);
 	for (int i = 0; i < cores; i++)
-		fscanf(cache, "%d", &tmp[i]);
+		fscanf(cache, "%d", &cachedCoreIdleTimes[i]);
 	rewind(cache);
 	fprintf(cache, "%d %d", coreStats.elapsedTime, coreStats.idleTime);
 	for (int i = 0; i < cores; i++)
 		fprintf(cache, " %d", coreStats.idleTimes[i]);
 	fclose(cache);
 
+	// get the difference
 	coreStats.elapsedTime -= cacheTime;
 	coreStats.idleTime -= cacheIdle;
 	for (int i = 0; i < cores; i++)
-		coreStats.idleTimes[i] -= tmp[i];
+		coreStats.idleTimes[i] -= cachedCoreIdleTimes[i];
 
 	return 0;
 }
 
+
 char *getLine(FILE *fp)
 {
-	// this breaks on files that don't end with a newline, but that's not a problem in linux
+	// this breaks on files that don't end with a newline, but that's
+	// not a problem in linux
+	// (and it doesn't make sense for windows users to use this code)
 	int i = 0;
 	while ((line[i] = fgetc(fp)) != '\n' && i < MAXLINE)
 		i++;
