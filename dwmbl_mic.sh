99570e7886162df53bfd285993eb6e024339830c
Jasper Shovelton <Beanie.github@proton.me>
2023-06-17 19:18:49 +0100

    modify to use wpctl instead of pactl

--- dwmbl_mic.sh
+++ dwmbl_mic.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-muted=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{ print $2 }')
-volume=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk '{ if(NR == 1) print ($5 + $12) / 2 }')
-if [ "$muted" = "yes" ]; then
-	echo "🎤 [Muted] $volume%"
-else
-	echo "🎤 $volume%"
-fi
+muted=$(wpctl get-volume @DEFAULT_SOURCE@ | awk '{ print $3 }')
+volume=$(wpctl get-volume @DEFAULT_SOURCE@ | awk '{ print $2 * 100 }')
 
-dunstify -t 1000 -h string:x-dunst-stack-tag:mic-volume-change "Mic volume" "Muted: $muted\nVolume: $volume%"
+if [ "$muted" != "" ]; then
+	# this makes the spacing look nicer
+	muted=" $muted"
+fi
+echo "🎤$muted $volume%"
+
+dunstify -t 1000 -h string:x-dunst-stack-tag:mic-volume-change "Mic volume" "Volume: $volume%$muted"
--- dwmbl_vol.sh
+++ dwmbl_vol.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{ print $2 }')
-volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{ if(NR == 1) print ($5 + $12) / 2 }')
-if [ "$muted" = "yes" ]; then
+muted=$(wpctl get-volume @DEFAULT_SINK@ | awk '{ print $3 }')
+volume=$(wpctl get-volume @DEFAULT_SINK@ | awk '{ print $2 * 100 }')
+if [ "$muted" == "[MUTED]" ]; then
 	echo "🔇 $volume%"
+        # this makes the spacing look nicer
+	muted=" $muted"
 elif [ $volume -lt 20 ]; then
 	echo "🔈 $volume%"
 elif [ $volume -lt 50 ]; then
@@ -12,4 +14,4 @@ else
 	echo "🔊 $volume%"
 fi
 
-dunstify -t 1000 -h string:x-dunst-stack-tag:speaker-volume-change "Speaker volume" "Muted: $muted\nVolume: $volume%"
+dunstify -t 1000 -h string:x-dunst-stack-tag:speaker-volume-change "Speaker volume" "Volume: $volume%$muted"
